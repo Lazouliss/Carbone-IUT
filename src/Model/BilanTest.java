81e8036c518ae8093489e9644e9c502ae9c048cd
Wassim Boukhari <wassim.boukhari@universite-paris-saclay.fr>
2023-06-22 22:32:44 +0200

    move du bilan test

--- src/Model/BilanTest.java
+++ src/Model/BilanTest.java
@@ -28,7 +28,6 @@ public class BilanTest {
 	public void TestaddCasGeneral(){
 
 		this.bilan.reset();//= new Bilan();
-		
 		this.bilan.add("Nourriture", 0, 50);
 
 		assertEquals(50, bilan.getMinCO2(), 0);
@@ -38,7 +37,7 @@ public class BilanTest {
 
 
 	}
-	
+
 	@Test
 	public void TestaddLimite(){
 
@@ -48,28 +47,28 @@ public class BilanTest {
 
 
 		assertEquals(2147483647, bilan.getMinCO2(), 0);
-        assertEquals(2147483647, bilan.getMaxCO2(), 0);
-        //assertEquals(2147483647, bilan.getMoyCO2(), 0);
+        	assertEquals(2147483647, bilan.getMaxCO2(), 0);
+        	//assertEquals(2147483647, bilan.getMoyCO2(), 0);
 
 
 	}
 
 	@Test
 	public void TestaddValueZero(){
-    	this.bilan.reset();//= new Bilan();
+    		this.bilan.reset();//= new Bilan();
 		assertEquals(0, bilan.getMinCO2(),0);
-        assertEquals(0, bilan.getMaxCO2(),0);
-        //assertEquals(0, bilan.getMoyCO2(),0);
+	        assertEquals(0, bilan.getMaxCO2(),0);
+        	//assertEquals(0, bilan.getMoyCO2(),0);
 
 
 	}
-	
+
     @Test
 	public void TestaddValueNeg(){
-	    this.bilan.reset();
-	    double tmaxC02=bilan.getMaxCO2();
-	    double tminC02=bilan.getMinCO2();
-	    double tmoyC02=bilan.getMoyCO2();
+		this.bilan.reset();
+	    	double tmaxC02=bilan.getMaxCO2();
+	    	double tminC02=bilan.getMinCO2();
+	    	double tmoyC02=bilan.getMoyCO2();
 		this.bilan.add("Nourriture", 0, -10 );
 		assertEquals(tmaxC02,bilan.getMaxCO2(), 0);
 		assertEquals(tminC02, bilan.getMinCO2(), 0);
@@ -78,25 +77,24 @@ public class BilanTest {
 
 	@Test
 	public void TestNomInvalide(){
-	    this.bilan.reset();
-	    double tmaxC02=bilan.getMaxCO2();
-	    double tminC02=bilan.getMinCO2();
-	    double tmoyC02=bilan.getMoyCO2();
+	   	this.bilan.reset();
+	    	double tmaxC02=bilan.getMaxCO2();
+	    	double tminC02=bilan.getMinCO2();
+	    	double tmoyC02=bilan.getMoyCO2();
 		this.bilan.add("toto", 0, 1);
 		assertEquals(tmaxC02,bilan.getMaxCO2(),0);
 		assertEquals(tminC02, bilan.getMinCO2(),0);
 		//assertEquals(tmoyC02, bilan.getMoyCO2(),0);
 	}
-	
+
 	@Test
 	public void TestIndiceCasGeneral(){
-	    this.bilan.reset();
-	    Data data= new Data();
-	    
+	    	this.bilan.reset();
+		Data data= new Data();
 		this.bilan.add("Transport", 1, 0);
 		assertEquals(data.getTransportMax()[1],bilan.getMaxCO2(),0);
 		assertEquals(data.getTransportMin()[1], bilan.getMinCO2(),0);
 		//assertEquals(tmoyC02, bilan.getMoyCO2(),0);
 	}
-	
+
 }
