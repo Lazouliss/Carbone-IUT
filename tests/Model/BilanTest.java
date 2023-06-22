b4c7dfb685a2f97093189bcb2a15a742e29c42fb
Wassim Boukhari <wassim.boukhari@universite-paris-saclay.fr>
2023-06-22 10:41:07 +0200

    final bilanTest

--- tests/Model/BilanTest.java
+++ tests/Model/BilanTest.java
@@ -42,10 +42,6 @@ public class BilanTest {
 	@Test
 	public void TestaddLimite(){
 
-		assertEquals(50, bilan.getMinCO2(),0);
-		assertEquals(50, bilan.getMaxCO2(),0);
-		assertEquals(50, bilan.getMoyCO2(),0);
-
 
 		this.bilan.reset();//= new Bilan();
 		this.bilan.add("Nourriture", 0, 2147483647);
@@ -61,34 +57,13 @@ public class BilanTest {
 	@Test
 	public void TestaddValueZero(){
     	this.bilan.reset();//= new Bilan();
-		assertEquals(2147483647, bilan.getMinCO2(),0);
-        assertEquals(2147483647, bilan.getMaxCO2(),0);
-        //assertEquals(2147483647, bilan.getMoyCO2(),0);
+		assertEquals(0, bilan.getMinCO2(),0);
+        assertEquals(0, bilan.getMaxCO2(),0);
+        //assertEquals(0, bilan.getMoyCO2(),0);
 
 
 	}
 	
-	@Test
-	public void TestaddValueZero(){
-	    double tmaxC02=bilan.getMaxCO2();
-	    double tminC02=bilan.getMinCO2();
-	    double tmoyC02=bilan.getMoyCO2();
- 
-		this.bilan.add("Nourriture", 0, 0 );
-
-		assertEquals(tmaxC02,bilan.getMaxCO2(), 0);
-		assertEquals(tminC02, bilan.getMinCO2(), 0);
-		//assertEquals(tmoyC02, bilan.getMoyCO2(), 0);
-	}
-    
-    @Test
-	public void TestaddValueNeg(){
-	    this.bilan.reset();//= new Bilan();
-
-		assertEquals(tmaxC02,bilan.getMaxCO2(),0);
-		assertEquals(tminC02, bilan.getMinCO2(),0);
-		//assertEquals(tmoyC02, bilan.getMoyCO2(),0);
-	}
     @Test
 	public void TestaddValueNeg(){
 	    double tmaxC02=bilan.getMaxCO2();
