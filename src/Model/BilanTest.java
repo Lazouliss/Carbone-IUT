643b0efa96d626e8fbad18021347b2d50a8a93c3
Thomas M <thomas.montigny@universite-paris-saclay.fr>
2025-11-02 23:07:22 +0100

    fix: fixed a bad old commit of a merge conflict

--- src/Model/BilanTest.java
+++ src/Model/BilanTest.java
@@ -23,43 +23,30 @@ public class BilanTest {
 		//assertEquals(0, bilan.getMoyCO2(),0);
 
 	}
-	
-@Test
+
+	@Test
 	public void TestaddCasGeneral(){
 
-<<<<<<< HEAD:tests/Model/BilanTest.java
-		bilan.reset();//= new Bilan();
-		bilan.add("Nourriture", 0, 50);
-=======
 		this.bilan.reset();//= new Bilan();
 		this.bilan.add("Nourriture", 0, 50);
 
->>>>>>> Wassim-bilanTest:src/Model/BilanTest.java
 		assertEquals(50, bilan.getMinCO2(), 0);
 		assertEquals(50, bilan.getMaxCO2(), 0);
 		//assertEquals(50, bilan.getMoyCO2(), 0);
+
+
+
 	}
 
 	@Test
 	public void TestaddLimite(){
 
-		bilan.reset();//= new Bilan();
-		bilan.add("Nourriture", 0, 2147483647);
-		assertEquals(2147483647, bilan.getMinCO2(), 0);
-<<<<<<< HEAD:tests/Model/BilanTest.java
-        assertEquals(2147483647, bilan.getMaxCO2(), 0);
-        assertEquals(2147483647, bilan.getMoyCO2(), 0);
-	}
 
-	
-    @Test
-	public void TestaddValueNeg(){
-	    bilan.reset();
-	    double tmaxC02=bilan.getMaxCO2();
-	    double tminC02=bilan.getMinCO2();
-	    double tmoyC02=bilan.getMoyCO2();
-		bilan.add("Nourriture", 0, -10 );
-=======
+		this.bilan.reset();//= new Bilan();
+		this.bilan.add("Nourriture", 0, 2147483647);
+
+
+		assertEquals(2147483647, bilan.getMinCO2(), 0);
         	assertEquals(2147483647, bilan.getMaxCO2(), 0);
         	//assertEquals(2147483647, bilan.getMoyCO2(), 0);
 
@@ -83,14 +70,11 @@ public class BilanTest {
 	    	double tminC02=bilan.getMinCO2();
 	    	double tmoyC02=bilan.getMoyCO2();
 		this.bilan.add("Nourriture", 0, -10 );
->>>>>>> Wassim-bilanTest:src/Model/BilanTest.java
 		assertEquals(tmaxC02,bilan.getMaxCO2(), 0);
 		assertEquals(tminC02, bilan.getMinCO2(), 0);
-		assertEquals(tmoyC02, bilan.getMoyCO2(), 0);
+		//assertEquals(tmoyC02, bilan.getMoyCO2(), 0);
 	}
 
-<<<<<<< HEAD:tests/Model/BilanTest.java
-=======
 	@Test
 	public void TestNomInvalide(){
 	   	this.bilan.reset();
@@ -136,7 +120,5 @@ public class BilanTest {
 
 
 	}
->>>>>>> Wassim-bilanTest:src/Model/BilanTest.java
 
 }
-
